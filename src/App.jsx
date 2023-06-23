dc377c1b6924ce476222eee9d1b26e4c0e0cef5d
Muhammad-Ahmed94 <muneebbum727@gmail.com>
2023-06-23 16:52:14 +0500

    1st commit, successfully made API request, and getting back response

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import apiRequest from "./componoents/api";
+apiRequest();
+function App() {
+  return <div>Hello!</div>;
+}
+
+export default App;
---
+++ src/componoents/api.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiRequest = async () => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    headers: {
+      Authorization: "Client-ID QJjxiwYXTyiGzAiic5As2nDFIgD9aUxnz9otbNtsB-s",
+    },
+    params: {
+      query: "cars",
+    },
+  });
+  console.log(response);
+  return response;
+};
+
+export default apiRequest;
